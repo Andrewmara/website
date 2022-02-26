21eff92f73fa7a7166cb98597248514698d90be1
Andrew Marasigan <mcjoshuaandrew@gmail.com>
2022-02-26 18:13:03 +0000

    +js

---
+++ jam.js
@@ -0,0 +1,10 @@
+const name = document.getElementById("myname");
+setInterval(function() {
+    document.getElementById("myname").innerHTML = "Joshua"
+}, 1000);
+setInterval(function() {
+    document.getElementById("myname").innerHTML = "Andrew"
+}, 2000);
+setInterval(function() {
+    document.getElementById("myname").innerHTML = "Marasigan"
+}, 3000);
